5ff004b0a9ad2893e039687e898be08fcc23f4bc
Dominic Skinner <ds672@unicri.damtp.cam.ac.uk>
2016-07-04 17:26:41 +0100

    Reproduced Tim's graphs

--- Graphs-and-Data/graph3.m
+++ Graphs-and-Data/graph3.m
@@ -5,7 +5,8 @@ figure('units','normalized','outerposition',[0 0 1 1]) % Makes figure fill
 ax = gca;
 hold on
 
-axis( [0.0, 0.3, 0.3,0.4]);
+axis( [0.0, 0.01, 0.35,0.38]);
+axis square
 xlabel(ax,'$ x $','Interpreter','latex','fontsize',25);
 ylabel(ax,'$ h''x^{1/3} $','Interpreter','latex','fontsize',25);
 title(ax,'Plot of $x$ against $h''x^{1/3}$',...
@@ -13,21 +14,23 @@ title(ax,'Plot of $x$ against $h''x^{1/3}$',...
 set(ax,'TickLabelInterpreter', 'latex');
 set(ax,'fontsize',20')
 set(get(ax,'YLabel'),'Rotation',0)
-set(get(ax,'YLabel'),'Rotation',0, 'Position', [-0.02, 0.35])
+set(get(ax,'YLabel'),'Rotation',0, 'Position', [-0.0014, 0.365])
 
 clear data
-load n800x40.mat
-dat = 1:300;
-plot(ax,x(dat),hprime_data(dat,2)'.*x(dat).^(-1/6),...
-    x(dat),hprime_data(dat,6)'.*x(dat).^(-1/6), ...
-x(dat),hprime_data(dat,14)'.*x(dat).^(-1/6))
+load n400x40.mat
+dat = 1:30;
+plot(ax,x(dat),hprime_data(401:430,9)'.*x(dat).^(-1/6), '*-', ...
+    x(dat),hprime_data(400+dat,10)'.*x(dat).^(-1/6),'*-', ...
+            x(dat),hprime_data(400+dat,11)'.*x(dat).^(-1/6),'*-', ...
+x(dat),hprime_data(400+dat,12)'.*(x(dat).^(-1/6)),'*-')
 s = '$K_I$ =';
-s1 = strcat(s, num2str(2*sqrt(3*pi)*KI(2)));
-s2 = strcat(s, num2str(2*sqrt(3*pi)*KI(6)));
-s3 = strcat(s, num2str(2*sqrt(3*pi)*KI(14)));
+s1 = strcat(s, num2str(3*sqrt(2*pi)*KI(9)));
+s2 = strcat(s, num2str(3*sqrt(2*pi)*KI(10)));
+s3 = strcat(s, num2str(3*sqrt(2*pi)*KI(11)));
+s4 = strcat(s, num2str(3*sqrt(2*pi)*KI(12)));
 
-legend({s1,s2,s3},'Interpreter','latex')
+legend({s1,s2,s3,s4},'Interpreter','latex')
     
 
 
-export_fig ('hprime-x', '-png', '-transparent','-m1.5')
+%export_fig ('hprime-x', '-png', '-transparent','-m1.5')
