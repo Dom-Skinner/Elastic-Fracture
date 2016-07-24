5035e9523ee0e4fec7c346483115215591d6ab49
Dominic Skinner <ds672@unicri.damtp.cam.ac.uk>
2016-07-24 16:34:11 +0100

    minor changes

--- linear_perturbation_problem/untitled3.m
+++ linear_perturbation_problem/untitled3.m
@@ -1,21 +1,32 @@
 clear
+load n400x40-extended
+
+clear
 load n400x30-extended
 K = 3*sqrt(2*pi)*KI;
 s = 0.138673;
-u = 4 - 6*s;
 l0 = 0.05943;
 D = -0.00809;
-z = tan((0.5:1:n-1.5)*atan(sqrt(xmax))/(n-1)).^2;
-
-[h0_prime,~] = interpolate_hprime(x,n,hprime_data,K);
 
 
+[~,h0_prime] = interpolate_hprime(x,n,hprime_data,K);
 h_coefficient_matrix = hprime_to_h_s(x,0.5);
 h0 = h_integrate(h0_prime',x,n,t,h_coefficient_matrix,0.5);
 
-h0_prime_s = convert(0.5,2/3,n,t,x,h0_prime);
-h_coefficient_matrix_s = hprime_to_h_s(x,2/3);
+h1_prime = find_h1_prime(n,hprime_data,K,h0_prime);
 
-h0_s = h_integrate(h0_prime_s,x,n,t,h_coefficient_matrix_s,2/3);
+h_coefficient_matrix_s = hprime_to_h_s(x,s);
 
-plot(x,h0,x,h0_s)
+
+Hprime_tilde = h0_prime - (3*l0/D)*h1_prime;
+
+Hprime_tilde_s = convert(0.5,s,n,t,x,Hprime_tilde);
+
+
+p1 = polyfit(x(25:35) , Hprime_tilde_s(25:35)',1);
+Hprime_tilde_s(1:20)=p1(1).*x(1:20)+p1(2);
+
+
+
+plot(x,Hprime_tilde_s,'o',x,Hprime_tilde_u)
+axis([0,0.1,0.05,0.2])
