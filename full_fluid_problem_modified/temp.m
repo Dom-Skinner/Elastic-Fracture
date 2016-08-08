031ae118e9f1e08c8064aca3e9be3aaf0c2367fe
Dominic Skinner <ds672@unicri.damtp.cam.ac.uk>
2016-08-08 17:18:22 +0100

    Some serious data files created

--- full_fluid_problem_modified/temp.m
+++ full_fluid_problem_modified/temp.m
@@ -1,6 +1,10 @@
-clear 
-load n477x768
-plot((x),(hprime_data(n+1:end,8)-hprime_data(n+1,8)),'o-', ...
-    (x),(hprime_data(n+1:end,2)-hprime_data(n+1,2)),'o-',...
-    x,0.35*x.^(1/4),x,2*x.^(1/2),x,0.6*x.^(1/3),x,0.415*x.^(0.28))
-axis([0,0.045,0,0.18])
+n_val = [639, 720, 830, 962, 1124, 1286, 1440, 1661];
+x_val = [893, 906, 946, 921, 934, 944, 893, 940];
+s = 0.138673;
+
+for k = 1:numel(n_val)
+    file = strcat('n', num2str(n_val(k)), 'x', num2str(x_val(k)));
+    load(file)
+    [kernel_matrix, ~] = pressure_shear_matrix_s(x,z,s,t);
+    save(file, '-append', 'kernel_matrix','s');
+end
