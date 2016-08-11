5b350ed0b6182cd832246f171ba10d34c2bb6474
Dominic Skinner <ds672@unicri.damtp.cam.ac.uk>
2016-08-11 17:46:47 +0100

    Extended problem now fully working

--- full_fluid_problem_h_advance/scaled_fixed_lambda_M_iteration.m
+++ full_fluid_problem_h_advance/scaled_fixed_lambda_M_iteration.m
@@ -1,4 +1,4 @@
-function [KI, hprime_new,p] = scaled_fixed_lambda_M_iteration(x,z,v,w,t,xmax, lambda, tol, hprime_start)
+function [KI,KII, hprime_new,p] = scaled_fixed_lambda_M_iteration(x,z,v,w,t,xmax, lambda, tol, hprime_start)
 % Function that iterates, finding K for some given value of lambda,
 % up to some tolerance, tol. 
 
@@ -11,15 +11,17 @@ function [KI, hprime_new,p] = scaled_fixed_lambda_M_iteration(x,z,v,w,t,xmax, la
 nx = numel(x);
 nv = numel(v);
 vt = t + nv-nx;
-
+L = v(1+nv-nx);
 
 %finds the appropriate elasticity kernel
-K11 = pressure_matrix_g(v,w(end-nx+2:end),vt,lambda);
-K12 = pressure_matrix_h(x,z,t,lambda);
+%K11 = pressure_matrix_g(v,w(end-nx+2:end),vt,lambda);
+%K12 = pressure_matrix_h(x,z,t,lambda);
+K11 = pressure_matrix_g(v,w,vt,lambda);
+K12 = pressure_matrix_h(x,w-L,t,lambda);
 K21 = shear_matrix_g(v,w,vt,lambda);
-K22 = shear_matrix_h(x,w-2.2,t,lambda);
+K22 = shear_matrix_h(x,w-L,t,lambda);
 
-kernel_matrix = [K11,K12 ; K21,K22];
+kernel_matrix = [K11(1+nv-nx:end,:),K12(1+nv-nx:end,:) ; K21,K22];
 
 
 %in short, we wish to solve Ah = f(h), where f is our map from h' to p.
@@ -47,7 +49,7 @@ conditioning = rcond(A);
 %saves a matrix to convert h' to h
 h_coefficient_matrix = hprime_to_h(x,t,lambda);
 
-fprintf('\n n = %d xmax = %d lambda = %6.4g \n', nv, xmax, lambda)
+fprintf('\n n = %d xmax = %d lambda = %6.4g L = %4.4g\n', nv, xmax, lambda,L)
 fprintf(' condition number = %6.4e \n', conditioning)
 fprintf('iteration,  KI,       dKI,         KII,          dKII           global diff\n');
 
@@ -64,6 +66,9 @@ while abs(KI_diff) >= tol && iteration_number<20
 
     [p,dp] = scaled_hprime_to_p(x,z,hprime_old(nv+1:end),lambda,h_coefficient_matrix,nv+nx,t);
     hprime_new = hprime_old + (A-dp)\(p-A*hprime_old);
+    %p1 = K11*hprime_new(1:nv);
+    %p2 = K12*hprime_new(nv+1:end);
+    %p = [K11, K12]*hprime_new;
     
     KI_new = hprime_new(nv+1);
     KII_new = hprime_new(1);
@@ -77,7 +82,8 @@ while abs(KI_diff) >= tol && iteration_number<20
    
 end
 
-KI = KI_new;
+KI  = KI_new;
+KII = KII_new;
 if iteration_number == 20
     fprintf('Failure to coverge\n');
     KI=-1;
